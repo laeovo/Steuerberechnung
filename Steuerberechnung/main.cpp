4cd7dade9f81636a1e81c460c6c775c727d2b793
Leo <leo.vornberger@gmx.de>
2020-08-30 15:30:43 +0200

    Initial Commit

---
+++ Steuerberechnung/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Steuerberechnung
+//
+//  Created by Leo Vornberger on 30.08.20.
+//  Copyright © 2020 Leo Vornberger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
